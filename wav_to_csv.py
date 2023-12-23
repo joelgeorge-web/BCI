d2517aa6ed111c9bf1e39282b22d66f50c81958c
joelgerge-web <joelgeorge98765@gmail.com>
2023-12-23 13:02:19 +0530

    edited data files

--- wav_to_csv.py
+++ wav_to_csv.py
@@ -1,31 +1,13 @@
-# import sys, os, os.path
-# from scipy.io import wavfile
-# import pandas as pd
-
-# input_filename = 'BYB_Recording_2023-12-13_11.47.15.wav'
-# if input_filename[-3:] != 'wav':
-#     print('WARNING!! Input File format should be *.wav')
-#     sys.exit()
-
-# sample_rate, data = wavfile.read(str(input_filename))
-# print('Load is Done! \n')
-# wavData = pd.DataFrame(data)
-# print('Multi channel .wav file\n')
-# print('number of channel : ' + str(len(wavData.columns)))
-# wavData.to_csv(str(input_filename[:-4] + "Output_multi_channel.csv"), mode='w')
-# print('Save is done ' + str(input_filename[:-4]) + 'Output_multi_channel.csv')
-
-
 import sys
 import os
 from scipy.io import wavfile
 import pandas as pd
 
 # Specify the folder containing WAV files
-input_folder = 'wavfile'
+input_folder = r'C:\Users\Joel\OneDrive\Documents\BYB'
 
 # Create an "output" folder if it doesn't exist
-output_folder = 'output_beta'
+output_folder = 'output_23-12-2023'
 os.makedirs(output_folder, exist_ok=True)
 
 # Process all WAV files in the input folder
